1a5495da607f070efd37bffdad6a88e652596cc3
Yasmin Sales <teacheryasminsales@gmail.com>
2021-06-13 18:38:36 -0300

    Exercício Linguagem de Programação 13.6

---
+++ OperacoesMatematicas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculadora_console
+{
+    class OperacoesMatematicas
+    { 
+        public int Soma(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        public int Subtracao(int numero1, int numero2)
+        {
+            return numero1 - numero2; 
+        }
+
+        public int Multiplicacao(int numero1, int numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        public int Divisao(int numero1, int numero2)
+        {
+            return numero1 / numero2; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace calculadora_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2, resultado;
+            string operacao;
+
+            Console.WriteLine("Digite um número: ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a operação da conta: Soma, Subtração, Multiplicação ou Divisão? ");
+            operacao = Console.ReadLine();
+
+            Console.WriteLine("Digite um outro número: ");
+            numero2 = int.Parse(Console.ReadLine());
+
+            var res = new OperacoesMatematicas();
+
+            switch (operacao)
+            {
+                case "Soma":
+                 resultado = res.Soma(numero1, numero2);
+                 Console.WriteLine("O resultado é: " + resultado);
+                 break;
+
+                case "Subtração":
+                 resultado = res.Subtracao(numero1, numero2);
+                 Console.WriteLine("O resultado é: " + resultado);
+                 break;
+
+                case "Multiplicação":
+                 resultado = res.Multiplicacao(numero1, numero2);
+                 Console.WriteLine("O resultado é: " + resultado);
+                 break;
+
+                case "Divisão":
+                 resultado = res.Divisao(numero1, numero2);
+                 Console.WriteLine("O resultado é: " + resultado);
+                 break;
+
+                default:
+                    Console.WriteLine("Ocorreu um erro.");
+                    break;
+            }
+
+            Console.ReadKey();
+            
+        }
+
+      
+    }
+}
